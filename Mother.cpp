1d4747ebc111f25df9eb891b07c700eeb2c720a2
Muse.Morgan <musemorgan@yahoo.com>
2024-02-03 17:19:24 -0500

    first commit

---
+++ Mother.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+
+{
+string Name;
+
+cout<<"What is your mother's name? "<< endl;
+cin >> Name;
+cout <<Name<<" is your mother's name! That's so nice!"<<endl; 
+
+
+
+
+    return 0;
+}
