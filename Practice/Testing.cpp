f8756fb7fe9604b82a430fc1cfd646c27b7cb125
Muse.Morgan <musemorgan@yahoo.com>
2024-02-03 17:47:55 -0500

    commits

---
+++ Practice/Testing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+cout<< "This is just to test compiling statements. "<<endl;
+cout<<"The instructions are written in the code as statments!! Enjoy! "<<endl;
+//First, create a .cpp file by creating a file and naming it "[whatever name you would like].cpp" and INCLUDE .cpp at the end
+//Then, go into the terminal and do "cd [name of your file]"
+// cd will allow you to move your file into the directory 
+// Lastly, type in the compile statment "g++ -o [name you would like to make your execution file] [name of your file.cpp]"
+// now, type in ./[name of the run file you create in line 13]
+//and your done!!
+
+    return 0;
+
+}
