447a14d0b3710c5419ec1620cb6e7fab158a941c
Muse.Morgan <musemorgan@yahoo.com>
2024-02-03 21:13:31 -0500

    Operand

--- Practice/operands.cpp
+++ Practice/operands.cpp
@@ -7,8 +7,8 @@ int main ()
 {
 int a, b, c;
 
-cout << "(13.2 + 9.76) / 4 = " << (13.2 + 9.76) / 4 << endl;
-
-
+cout << "Lets evalute (13.2 + 9.76) / 4. The answer is = " << (13.2 + 9.76) / 4 << endl;
+cout<<"Also, 15/2 = "<< static_cast<double>(29)/2 <<endl;
+//adding paranthesis to the first number makes the answer into a mixed expression which allows for the answer to become a floating point number
     return 0;
 }
