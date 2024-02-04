a03f8e017697ebbbaf4992d459d80a0accc05eb3
Muse.Morgan <musemorgan@yahoo.com>
2024-02-04 12:01:30 -0500

    Committing changes and chap 2 excersize

---
+++ Practice/chap2ex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout<< "CCCCCCCCC         ++                ++"<<endl;
+cout<< "CC                ++                ++"<<endl;
+cout<< "CC          ++++++++++++++    ++++++++++++++"<<endl;
+cout<< "CC          ++++++++++++++    ++++++++++++++"<<endl;
+cout<< "CC                ++                ++"<<endl;
+cout<< "CCCCCCCCC         ++                ++"<<endl;
+
+
+    return 0;
+}
