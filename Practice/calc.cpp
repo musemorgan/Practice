e9660c5531476544e9e888e9fe460f9e21c7f5e0
Muse.Morgan <musemorgan@yahoo.com>
2024-02-05 02:11:53 -0500

    calculator hw

---
+++ Practice/calc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+    cout<<"Enter your first integer below: "<< endl;
+    cin>> a;
+    cout<<"Enter your second integer below: "<<endl;
+    cin>> b;
+    cout<< " Let's find the product of these values: " << a << " * " << b << " = "<< a*b<<endl;
+
+
+    return 0;
+}
