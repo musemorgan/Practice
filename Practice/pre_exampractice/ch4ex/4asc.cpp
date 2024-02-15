d9a8242657a28ab62f45c939a648ed655220e5ee
Muse.Morgan <musemorgan@yahoo.com>
2024-02-15 14:38:15 -0500

    INCOMPLETE

---
+++ Practice/pre_exampractice/ch4ex/4asc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main ()
+{
+int a, b, c;
+//char numberType;
+
+cout << "Please enter 3 numbers: " << endl;
+cin >> a >> b >> c;
+
+if (a>b) swap (a, b);
+if (b>c) swap (b,c);
+if (a>b) swap (a,b);
+//INCOMPLETE PROGRAM
+    return 0;
+}
