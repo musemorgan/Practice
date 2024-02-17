109db9708c49e4e7571ac697c3c245b30d4cae2c
Muse.Morgan <musemorgan@yahoo.com>
2024-02-17 16:26:04 -0500

    Attempting to list input in ascending order

--- Practice/pre_exampractice/ch4ex/4asc.cpp
+++ Practice/pre_exampractice/ch4ex/4asc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 
@@ -6,15 +7,69 @@ using namespace std;
 
 int main ()
 {
-int a, b, c;
-//char numberType;
+int num1, num2, num3;
+int largest, middle, smallest;
 
 cout << "Please enter 3 numbers: " << endl;
-cin >> a >> b >> c;
+cin >> num1 >> num2 >> num3;
 
-if (a>b) swap (a, b);
-if (b>c) swap (b,c);
-if (a>b) swap (a,b);
-//INCOMPLETE PROGRAM
+if (num1 > num2 && num1 > num3)
+    {
+        num1 = largest;
+        cout << "Number 1 is the largest interger. " << endl;
+    }
+else
+    if (num2 > num1 && num2 > num3)
+        {
+            num2 = largest;
+            cout << "Number 2 is the largest interger. " << endl;
+        }
+    else 
+        if (num3 > num1 && num3 > num2)
+            {
+                num3 = largest;
+                cout << "Number 3 is the largest integer. " << endl;
+            }
+
+if (num1 < num2 && num1 < num3)
+    {
+        num1 = smallest;
+        cout << "Number 1 is the smallest interger. " << endl;
+    }
+
+if (num2 < num1 && num2<num3)
+        {
+            num2 = smallest;
+            cout << "Number 2 is the smallest interger. " << endl;
+        }
+if (num3 < num1 && num3 < num2)
+        {
+          num3 = smallest;
+           cout << "Number 3 is the smallest integer. " << endl;
+        }
+
+
+
+if (num1 != smallest && num1!= largest)
+    {
+        num1 = middle;
+        cout << "Number 1 is the mid-range interger. " << endl;
+    }
+else
+    if (num2 != smallest && num2!=largest)
+        {
+            num2 = middle ;
+            cout << "Number 2 is the mid-range interger. " << endl;
+        }
+    else 
+        if (num3 != smallest && num3 !=largest)
+            {
+                num3 = middle;
+                cout << "num3 is the mid-range integer. "<< endl;
+            }
+
+cout << endl;
+
+cout << largest << ", "<< middle << ", "<< smallest << " is the order of the integers!" << endl;
     return 0;
 }
