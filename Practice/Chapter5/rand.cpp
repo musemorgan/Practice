d6188a390c1ce42d622ce4711ada40c362cd0da4
Muse.Morgan <musemorgan@yahoo.com>
2024-02-21 18:54:39 -0500

    random num gen //incomplete

--- Practice/Chapter5/rand.cpp
+++ Practice/Chapter5/rand.cpp
@@ -9,7 +9,7 @@ int main ()
 int num;
 cout << rand() << endl; //outputs a random number, but the same one is output each time the program is ran
 srand(time(0)) ;
- num = rand() % 100 ;
+
 
     return 0;
-}
+} //incomplete
