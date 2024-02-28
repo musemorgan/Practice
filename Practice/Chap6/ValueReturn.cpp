63671af3e793bcf10df1c514f0984271af2b42a8
Muse.Morgan <musemorgan@yahoo.com>
2024-02-28 13:22:58 -0500

    simple value returning function

---
+++ Practice/Chap6/ValueReturn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int addition();
+
+int main()
+{
+
+cout << addition() << " + 12 = ";
+cout << addition() + 12 << endl;
+    return 0;
+}
+
+int addition()
+{
+int x = 13;
+return x;
+}
