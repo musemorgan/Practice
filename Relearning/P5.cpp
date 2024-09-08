7a0ff0507d7757f1850b3d5bb23efde990ddd49e
Muse.Morgan <musemorgan@yahoo.com>
2024-09-08 11:55:53 -0400

    syntax

---
+++ Relearning/P5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+//working on syntax for classes
+using namespace std;
+
+class Device {
+    private:
+        int gigabyte {120};
+        bool cellular = true;
+       
+    public: 
+        Device () = default;
+        int password;
+        int applications;
+        
+
+};
+
+int main(){
+
+Device Ipad, Iphone, Macbook, Watch;
+
+
+
+
+    return 0;
+}
